60d9e032a995cb07096509bb7a308e14bd623f77
archiesingla <archiesingla22@gmail.com>
2025-06-20 18:35:12 -0300

    activity 5

---
+++ activity5/activity5/backend/utils/authMiddleware.py
@@ -0,0 +1,31 @@
+from functools import wraps
+from flask import request, jsonify
+import jwt
+
+SECRET_KEY = 'web_activity5'
+# Middleware
+def auth_middleware(f):
+    @wraps(f)
+    def middleware(*args, **kwargs):
+        # get the authorization header
+        header = request.headers.get('Authorization')
+
+        # if header is not available or it doesn't start with the Bearer
+        if not header or not header.startswith("Bearer "):
+            return jsonify({"error": "Unauthorized: Missing or invalid token"}), 401
+
+        # taking the token from header
+        token = header.split(" ")[1]
+
+
+        try:
+            decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            request.user = decoded
+        # if the token is invalid or is expired
+        except jwt.ExpiredSignatureError:
+            return jsonify({"error": "Token has expired"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Invalid token"}), 401
+
+        return f(*args, **kwargs)
+    return middleware
---
+++ activity5/activity5/frontend/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactCard from "../components/ContactCard";
+
+
+function Contact(){
+    return(
+        <>
+            <ContactCard />
+        </>
+    )
+}
+export default Contact;
